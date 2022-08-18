9a3b70b4a92ffbb38f496b913c150fa64a0af254
Raji-Chilakapati <111392021+Raji-Chilakapati@users.noreply.github.com>
2022-08-19 02:08:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+function myFunction(value){
+    console.log(value);
+    document.getElementById('container').innerHTML=value;
+}
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
